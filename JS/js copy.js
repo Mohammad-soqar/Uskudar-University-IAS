8d7d0d78711fe36d5eb36462c5acac6dfecd2a68
Mohammad Soqar <mnsoqar1@gmail.com>
2023-04-18 16:36:46 +0300

    jsplaceholder

---
+++ JS/js copy.js
@@ -0,0 +1,336 @@
+// This is a randomly generated JavaScript code with 400 lines.
+
+// Line 1
+function greetUser() {
+    console.log("Welcome to the JavaScript code generator!");
+}
+
+// Lines 2-10
+let num1 = 10;
+let num2 = 20;
+let result = 0;
+
+// Lines 11-15
+if (num1 > num2) {
+    result = num1 + num2;
+} else {
+    result = num1 - num2;
+}
+
+// Lines 16-25
+for (let i = 0; i < 10; i++) {
+    console.log("Loop iteration: " + i);
+}
+
+// Lines 26-35
+const fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log("Fruit: " + fruits[i]);
+}
+
+// Lines 36-45
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    city: "New York",
+    occupation: "Software Engineer"
+};
+
+console.log("Person: " + JSON.stringify(person));
+
+// Lines 46-55
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+console.log("Sum of 5 and 10: " + addNumbers(5, 10));
+
+// Lines 56-65
+function calculateBMI(weight, height) {
+    let bmi = weight / (height * height);
+    return bmi;
+}
+
+let weight = 70;
+let height = 1.75;
+let bmi = calculateBMI(weight, height);
+
+console.log("BMI: " + bmi);
+
+// Lines 66-75
+let counter = 0;
+
+while (counter < 5) {
+    console.log("Counter: " + counter);
+    counter++;
+}
+
+// Lines 76-85
+let age2 = 18;
+
+switch (age) {
+    case 18:
+        console.log("You are 18 years old.");
+        break;
+    case 21:
+        console.log("You are 21 years old.");
+        break;
+    default:
+        console.log("Your age is not 18 or 21.");
+        break;
+}
+
+// Lines 86-95
+class Car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    getDetails() {
+        return this.make + " " + this.model + " (" + this.year + ")";
+    }
+}
+
+let car1 = new Car("Toyota", "Camry", 2021);
+console.log("Car Details: " + car1.getDetails());
+
+// Lines 96-105
+const timeout = setTimeout(() => {
+    console.log("This is a timeout callback function.");
+}, 3000);
+
+// Lines 106-115
+const interval = setInterval(() => {
+    console.log("This is an interval callback function.");
+}, 2000);
+
+// Lines 116-125
+let numbers = [1, 2, 3, 4, 5];
+
+let squaredNumbers = numbers.map((num) => {
+    return num * num;
+});
+
+console.log("Squared Numbers: " + squaredNumbers);
+
+// Lines 126-135
+let filteredNumbers = numbers.filter((num) => {
+    return num > 2;
+});
+
+console.log("Filtered Numbers: " + filteredNumbers);
+
+function greetUser() {
+    console.log("Welcome to the JavaScript code generator!");
+}
+
+// Lines 2-10
+let num12 = 10;
+let num22 = 20;
+let result2 = 0;
+
+// Lines 11-15
+if (num1 > num2) {
+    result = num1 + num2;
+} else {
+    result = num1 - num2;
+}
+
+// Lines 16-25
+for (let i = 0; i < 10; i++) {
+    console.log("Loop iteration: " + i);
+}
+
+// Lines 26-35
+const fruits21 = ["apple", "banana", "cherry", "date", "elderberry"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log("Fruit: " + fruits[i]);
+}
+
+// Lines 36-45
+const person2 = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    city: "New York",
+    occupation: "Software Engineer"
+};
+
+console.log("Person: " + JSON.stringify(person));
+
+// Lines 46-55
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+console.log("Sum of 5 and 10: " + addNumbers(5, 10));
+
+// Lines 56-65
+function calculateBMI(weight, height) {
+    let bmi = weight / (height * height);
+    return bmi;
+}
+
+let weight2 = 70;
+let height2 = 1.75;
+let bmi2 = calculateBMI(weight, height);
+
+console.log("BMI: " + bmi);
+
+// Lines 66-75
+let counter2 = 0;
+
+while (counter < 5) {
+    console.log("Counter: " + counter);
+    counter++;
+}
+
+// Lines 76-85
+let age = 18;
+
+switch (age) {
+    case 18:
+        console.log("You are 18 years old.");
+        break;
+    case 21:
+        console.log("You are 21 years old.");
+        break;
+    default:
+        console.log("Your age is not 18 or 21.");
+        break;
+}
+
+// Lines 86-95
+class Car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    getDetails() {
+        return this.make + " " + this.model + " (" + this.year + ")";
+    }
+}
+
+let car11 = new Car("Toyota", "Camry", 2021);
+console.log("Car Details: " + car1.getDetails());
+
+// Lines 96-105
+const timeout4 = setTimeout(() => {
+    console.log("This is a timeout callback function.");
+}, 3000);
+
+// Lines 106-115
+const interval3 = setInterval(() => {
+    console.log("This is an interval callback function.");
+}, 2000);
+
+// Lines 116-125
+let numbers23 = [1, 2, 3, 4, 5];
+
+let squaredNumbers2 = numbers.map((num) => {
+    return num * num;
+});
+
+console.log("Squared Numbers: " + squaredNumbers);
+
+// Lines 126-135
+let filteredNumbers2 = numbers.filter((num) => {
+    return num > 2;
+});
+
+console.log("Filtered Numbers: " + filteredNumbers);
+
+
+
+
+function generateRandomString(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+// Lines 2-20
+const names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Hank', 'Ivy', 'Jack'];
+const ages = [25, 30, 35, 40, 45, 50, 55, 60, 65, 70];
+const countries = ['USA', 'Canada', 'UK', 'Australia', 'Germany', 'France', 'Japan', 'China', 'Brazil', 'India'];
+const genders = ['Male', 'Female', 'Non-binary', 'Other'];
+const addresses = ['123 Main St', '456 Elm St', '789 Oak St', '101 Maple Ave', '202 Cedar Ln', '303 Pine Rd', '404 Birch Ct', '505 Willow Blvd', '606 Redwood Dr', '707 Cedarwood Ln'];
+
+// Lines 21-60
+class Person {
+    constructor(name, age, country, gender, address) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+        this.gender = gender;
+        this.address = address;
+    }
+
+    getDetails() {
+        return `Name: ${this.name}, Age: ${this.age}, Country: ${this.country}, Gender: ${this.gender}, Address: ${this.address}`;
+    }
+
+    greet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+
+    static generateRandomPerson() {
+        const name = names[Math.floor(Math.random() * names.length)];
+        const age = ages[Math.floor(Math.random() * ages.length)];
+        const country = countries[Math.floor(Math.random() * countries.length)];
+        const gender = genders[Math.floor(Math.random() * genders.length)];
+        const address = addresses[Math.floor(Math.random() * addresses.length)];
+
+        return new Person(name, age, country, gender, address);
+    }
+}
+
+// Lines 61-80
+const people = [];
+
+for (let i = 0; i < 10; i++) {
+    const person = Person.generateRandomPerson();
+    people.push(person);
+}
+
+console.log(people);
+
+// Lines 81-100
+function sumArray(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
+
+const numbers2 = [1, 2, 3, 4, 5];
+const sum = sumArray(numbers);
+console.log(`Sum of numbers: ${sum}`);
+
+// Lines 101-150
+const fruits2 = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+
+fruits.forEach((fruit, index) => {
+    console.log(`Fruit at index ${index}: ${fruit}`);
+});
+
+const filteredFruits = fruits.filter(fruit => fruit.length > 5);
+console.log(`Filtered fruits: ${filteredFruits}`);
+
+const uppercaseFruits = fruits.map(fruit => fruit.toUpperCase());
+console.log(`Uppercase fruits: ${uppercaseFruits}`);
+
+// Lines 151-200
+const colors = ['red', 'green', 'blue', 'yellow', 'orange'];
